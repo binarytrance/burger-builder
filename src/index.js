8f3bd50345116462fbde6a630741941c93168b80
binarytrance <ganeshan.dash@gmail.com>
2021-01-24 20:36:52 +0530

    installed thunk

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import "tailwindcss/dist/tailwind.css";
 // import "./assets/main.css";
 import App from "./App";
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import * as serviceWorker from "./serviceWorker";
+import thunk from 'redux-thunk';
 
-const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(burgerBuilderReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
 
